c0f8da60e87f961678cffeb00a5070b978a04457
sakib9592 <sakibalhasan212715@gmail.com>
2026-07-23 21:07:17 +0600

    added course result tests

--- test.sh
+++ test.sh
@@ -3,3 +3,7 @@
 gcc -I modules tests.c modules.c -o tests.out
 chmod +x tests.out
 ./tests.out
+
+gcc -I modules test_module_result.c modules.c modulesResult.c -o test_module_result.out
+chmod +x test_module_result.out
+./test_module_result.out
