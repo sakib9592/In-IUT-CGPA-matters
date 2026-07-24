0aab7b1cce725b82325d6d5d34991c22a6d5a326
sakib9592 <sakibalhasan212715@gmail.com>
2026-07-24 19:55:55 +0600

    edited make.ps1

--- make.ps1
+++ make.ps1
@@ -1,2 +1,3 @@
 gcc -I modules main.c modules.c -o calculator.exe
 gcc -I modules main.c modules.c modulesResult.c -o calculator.exe
+gcc -I modules main.c modules.c modulesResult.c gpa.c -o calculator.exe
